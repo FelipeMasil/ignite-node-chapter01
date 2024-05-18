59ef67987e862523bbd1edfc01bac18f8a6ceff2
FelipeMasil <felipe.manoel.silva@gmail.com>
2024-05-18 12:13:10 +0000

    Estrutura da aplicação

--- src/server.js
+++ src/server.js
@@ -1,7 +1,39 @@
-import http from 'node:http';
+import http from 'node:http'; //usar o prefixo "node:" para módulos internos no Nodejs
+
+/**
+ *      MÉTODOS DE REQUISIÇÃO
+ *      
+ *      GET => Buscar recurso no backend
+ *      POST => Criar um recurso no backend
+ *      PUT => Atualizar um recurso no backend
+ *      PATCH => Atualizar uma informação específica de um recurso no backend
+ *      DELETE => Deletar um recurso no backend
+ * 
+ */
+
+const users = []
 
 const server = http.createServer((req, res)=>{
-    return res.end('Hello')
+    const { method, url } = req;
+
+    if(method === 'GET' && url === '/users'){
+        return res
+        .setHeader('Content-type', 'application/json')
+        .end(JSON.stringify(users))
+    }
+
+    if(method === 'POST' && url === '/users'){
+        
+        users.push({
+            id: 1,
+            name: 'John Doe',
+            email: 'john.doe@mail.com'
+        })
+
+        return res.writeHead('201').end();
+    }
+
+    return res.writeHead('404').end();
 })
 
 server.listen(3333)
