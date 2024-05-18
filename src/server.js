a5299cf10e082b7d1a2f7490fb0aa54a95d2de36
FelipeMasil <felipe.manoel.silva@gmail.com>
2024-05-18 19:37:22 +0000

    Banco de dados local

--- src/server.js
+++ src/server.js
@@ -1,34 +1,22 @@
 import http from 'node:http'; //usar o prefixo "node:" para módulos internos no Nodejs
+
 import { json } from './middlewares/json.js';
 
-
-
-const users = []
+import { routes } from './routes.js'
 
 const server = http.createServer(async (req, res)=>{
     const { method, url } = req;
 
-
-
         await json(req, res)
 
-    if(method === 'GET' && url === '/users'){
-        return res
-        .end(JSON.stringify(users))
-    }
-
-    if(method === 'POST' && url === '/users'){
-        
-        const { name, email } = req.body;
-
-        users.push({
-            id: 1,
-            name,
-            email,
+        const route = routes.find(route =>{
+            return route.method === method && route.path === url
         })
+    
+        if (route){
+            return route.handler(req, res)
+        }
 
-        return res.writeHead('201').end();
-    }
 
     return res.writeHead('404').end();
 })
